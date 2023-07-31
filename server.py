a583420b4fb661f048df44259f3d345b4f7b5fee
ganbari <steinway.nathan@gmail.com>
2023-07-31 15:23:40 -0500

    completes sessions and carts

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, session
 import jinja2
 import melons
+import customers
+from forms import LoginForm
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route('/')
@@ -16,17 +19,89 @@ def all_melons():
 
 @app.route('/melon/<melon_id>')
 def melon_details(melon_id):
-    melon = melons.get_by_id()
+    melon = melons.get_by_id(melon_id)
     return render_template("melon_id.html", melon=melon )
 
 @app.route('/add_to_cart<melon_id>')
 
 def add_to_cart(melon_id):
-    return f"{melon_id} added to cart."
+
+    if 'username' not in session:
+        return redirect("/login")
+
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    cart = session['cart']
+
+    cart[melon_id] = cart.get(melon_id, 0) + 1
+    session.modified = True
+    flash(f"Melon {melon_id} successfully added to cart.")
+
+    return redirect('/cart')
 
 @app.route('/cart')
 def show_cart():
-    return render_template("cart.html")
+
+    if 'username' not in session:
+        return redirect("/login")
+    
+    order_total = 0
+    melon_storage = []
+
+    cart = session.get('cart', {})
+
+
+
+    for melon_id, quantity in cart.items():
+        melon = melons.get_by_id(melon_id)
+
+        total_cost = quantity * melon.price
+        order_total += total_cost
+
+        melon.quantity = quantity
+        melon.total_cost = total_cost
+
+        melon_storage.append(melon)
+
+
+    return render_template("cart.html", melon_storage=melon_storage, order_total=order_total)
+
+@app.route('/empty-cart')
+def empty_cart():
+    session["cart"] = {}
+
+    return redirect("/cart")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm(request.form)
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = customers.get_by_username(username)
+
+        if not user or user['password'] != password:
+            flash("Invalid username or password")
+            return redirect('/login')
+        
+        session["username"] = user["username"]
+        flash("Logged in.")
+        return redirect("/melons")
+    
+    return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout():
+
+    del session["username"]
+    flash("logged out.")
+    return redirect("/login")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("not_found.html")
 
 if __name__ == "__main__":
     app.env = "development"
